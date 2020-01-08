ffac250ad5410093a6fbca38f99036388b0d1375
Simon Gockner <simon.gockner@gmail.com>
2020-01-08 07:11:40 +0100

    - update version to 1.1

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 [assembly: AssemblyCulture ("")]
 
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 
